2d189c230218fd7d04d4baed34ad9f8f996a5afe
SeallEgg <seallegg@gmail.com>
2025-12-24 14:31:30 -0300

    update to newer module setup

--- hosts/enterprise/default.nix
+++ hosts/enterprise/default.nix
@@ -1,48 +1,18 @@
-{ self', lib, pkgs, modulesPath, ... }:
-
 {
+  lib,
+  modulesPath,
+  ...
+}: {
+  networking.hostName = "enterprise";
   system.stateVersion = "25.11";
-  imports =
-    [
-      (modulesPath + "/profiles/qemu-guest.nix")
-      ./partitioning.nix
-      ./options.nix
-    ];
 
-  boot.initrd.availableKernelModules = [ "ahci" "xhci_pci" "virtio_pci" "sr_mod" "virtio_blk" ];
-  boot.kernelModules = [ "kvm-amd" ];
-
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
-  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  networking.hostName = "armando";
-  networking.networkmanager.enable = true;
-  services.openssh.enable = true;
-
-  services.xserver.enable = true;
-  services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
-
-  services.printing.enable = true;
-
-  services.pipewire = {
-     enable = true;
-     pulse.enable = true;
-  };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  services.libinput.enable = true;
-
-  nixpkgs.config.allowUnfree = true;
-  programs.nh.enable = true;
-  programs.firefox.enable = true;
-
-  environment.systemPackages = with pkgs; [
-    micro
-    neovim
-    fastfetch
-    btop
-    kitty
+  imports = [
+    (modulesPath + "/profiles/qemu-guest.nix")
+    ./partitioning.nix
+    ./options.nix
   ];
 
+  boot.initrd.availableKernelModules = ["ahci" "xhci_pci" "virtio_pci" "sr_mod" "virtio_blk"];
+  boot.kernelModules = ["kvm-amd"];
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 }
