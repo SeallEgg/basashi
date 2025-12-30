3b03e865aa83cdadfe596208fe25b538d671f215
SeallEgg <seallegg@gmail.com>
2025-12-30 15:52:52 -0300

    added kernel configuration

---
+++ modules/core/kernel.nix
@@ -0,0 +1,27 @@
+{
+  lib,
+  pkgs,
+  config,
+  ...
+}: let
+  inherit (lib) mkOption types;
+  cfg = config.cfg.core.kernel;
+  kernelPackage =
+    if cfg == "lts"
+    then pkgs.linuxPackages
+    else if cfg == "latest"
+    then pkgs.linuxPackages_latest
+    else if cfg == "custom"
+    then pkgs.linuxPackages_zen # i know this isn't quite custom yet but i don't want to work on this right now
+    else throw "Kernel: Unknown kernel type!";
+in {
+  options.cfg.core.kernel = mkOption {
+    type = types.enum [
+      "latest"
+      "lts"
+      "custom"
+    ];
+    default = "latest";
+  };
+  config.boot.kernelPackages = kernelPackage;
+}
