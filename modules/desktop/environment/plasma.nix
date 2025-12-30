5133048e709a1ab76dfe4a838fe36f855b54fdd2
SeallEgg <seallegg@gmail.com>
2025-12-30 16:09:02 -0300

    make plasma toggleable option

--- modules/desktop/environment/plasma.nix
+++ modules/desktop/environment/plasma.nix
@@ -1,5 +1,19 @@
 {
-  services.xserver.enable = true;
-  services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
+  config,
+  lib,
+  pkgs,
+  ...
+}: let
+  inherit (lib) mkEnableOption mkIf;
+  cfg = config.cfg.desktop.environment.plasma;
+in {
+  options.cfg.desktop.environment.plasma = {
+    enable = mkEnableOption "KDE plasma and SDDM";
+  };
+
+  config = mkIf cfg.enable {
+    services.xserver.enable = true;
+    services.displayManager.sddm.enable = true;
+    services.desktopManager.plasma6.enable = true;
+  };
 }
