73a876c64cb784b1bd9759e55c8d84d6283fc14f
SeallEgg <seallegg@gmail.com>
2025-12-30 16:03:44 -0300

    add locale configuration

---
+++ modules/core/locale.nix
@@ -0,0 +1,10 @@
+{
+  i18n = {
+    defaultLocale = "en_DK.UTF-8";
+    extraLocales = "all"; # why not?
+    extraLocaleSettings = {
+      LC_MONETARY = "pt_BR.UTF-8";
+    };
+  };
+  time.timeZone = "America/Sao_Paulo";
+}
