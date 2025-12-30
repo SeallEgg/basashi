c301c04e04e38b83feed155996a333349c4ffa23
SeallEgg <seallegg@gmail.com>
2025-12-30 15:54:11 -0300

    massively expanded nix configuration & added automatic setup of the config

--- modules/core/nix.nix
+++ modules/core/nix.nix
@@ -1,6 +1,60 @@
 # such a stupid file name
 {
-  nix.settings.experimental-features = ["nix-command" "flakes"];
-  nixpkgs.config.allowUnfree = true;
-  programs.nh.enable = true;
+  lib,
+  pkgs,
+  inputs,
+  ...
+}: let
+  inherit (lib) mapAttrsToList;
+  inherit (builtins) mapAttrs;
+  nixPath = mapAttrsToList (n: _: "${n}=flake:${n}") inputs;
+in {
+  config = {
+    nix = {
+      channel.enable = false;
+      registry = mapAttrs (_: flake: {inherit flake;}) inputs;
+
+      inherit nixPath;
+
+      settings = {
+        nix-path = nixPath;
+        flake-registry = "";
+        experimental-features = ["nix-command" "flakes"];
+
+        allow-import-from-derivation = false;
+        auto-optimise-store = true;
+        use-xdg-base-directories = true;
+        extra-sandbox-paths = ["/var/cache/ccache"];
+        download-buffer-size = 1024 * 1024 * 1024;
+        http-connections = 50;
+        allowed-users = ["@wheel"];
+        trusted-users = ["@wheel"];
+
+        accept-flake-config = true;
+        extra-substituters = [
+          "https://nix-community.cachix.org"
+          "https://cache.nixos-cuda.org"
+        ];
+        extra-trusted-public-keys = [
+          "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+          "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
+        ];
+      };
+    };
+
+    programs.ccache = {
+      enable = true;
+      cacheDir = "/var/cache/ccache";
+    };
+
+    nixpkgs.config.allowUnfree = true;
+
+    programs.nh = {
+      enable = true;
+      clean.enable = true; # cleans weekly
+      clean.extraArgs = "--keep 3";
+    };
+
+    documentation.nixos.enable = false;
+  };
 }
