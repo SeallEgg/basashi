7462feba85af852311156f32c1bc54f4ae30ebaf
SeallEgg <seallegg@gmail.com>
2025-12-31 20:35:35 -0300

    separate sddm option

--- modules/desktop/environment/plasma.nix
+++ modules/desktop/environment/plasma.nix
@@ -8,11 +8,10 @@
   cfg = config.cfg.desktop.environment.plasma;
 in {
   options.cfg.desktop.environment.plasma = {
-    enable = mkEnableOption "KDE plasma and SDDM";
+    enable = mkEnableOption "KDE plasma";
   };
 
   config = mkIf cfg.enable {
-    services.xserver.enable = true;
     services.displayManager.sddm.enable = true;
     services.desktopManager.plasma6.enable = true;
   };
---
+++ modules/services/sddm.nix
@@ -0,0 +1,20 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: let
+  inherit (lib) mkEnableOption mkIf;
+  cfg = config.cfg.services.sddm;
+in {
+  options.cfg.services.sddm = {
+    enable = mkEnableOption "SDDM";
+  };
+
+  config = mkIf cfg.enable {
+    services.displayManager.sddm = {
+      enable = true;
+      wayland.enable = true;
+    };
+  };
+}
