0bad7cfcd31ccdf7507716ff2639ce0848efc0df
SeallEgg <seallegg@gmail.com>
2025-12-31 20:35:23 -0300

    move fonts module

---
+++ modules/desktop/theming/fonts.nix
@@ -0,0 +1,46 @@
+{
+  pkgs,
+  inputs,
+  config,
+  lib,
+  ...
+}: let
+  inherit (lib) mkEnableOption mkIf;
+  cfg = config.cfg.desktop.environment;
+in {
+  config = mkif cfg.niri.enable or cfg.plasma.enable {
+    imports = [inputs.apple-fonts.nixosModules.default];
+    fonts = {
+      apple-fonts.enable = true;
+      packages = with pkgs; [
+        noto-fonts
+        dejavu_fonts
+        freefont_ttf
+        gyre-fonts
+        liberation_ttf
+        unifont
+        corefonts
+        vista-fonts
+        maple-mono.NF
+      ];
+      fontconfig = {
+        defaultFonts = {
+          sansSerif = ["SF Pro"];
+          serif = ["New York"];
+          monospace = ["Maple Mono NF"];
+          emoji = ["Apple Color Emoji"];
+        };
+        antialias = true;
+        hinting = {
+          enable = true;
+          autohint = true;
+          style = "slight";
+        };
+        subpixel = {
+          rgba = "rgb";
+          lcdfilter = "default";
+        };
+      };
+    };
+  };
+}
